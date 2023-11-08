14f7b51b13efbc760adb58921d8d92f33b55d3dd
Christopher <crobles@nerdwallet.com>
2023-11-07 17:51:02 -0800

    Update page.tsx

--- app/wishlist/page.tsx
+++ app/wishlist/page.tsx
@@ -21,7 +21,7 @@ const getWishlist = async (): Promise<Sanity.Default.Schema.Wishlist> => {
         }
       }
     }`,
-    { revalidate: 30 }
+    { cache: 'no-store' }
   )
   return wishlist
 }
